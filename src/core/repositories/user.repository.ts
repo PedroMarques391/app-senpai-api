6b0fde4732f0e9cbaed9265f028b86947d645879
Pedro Marques <pedromarques391@gmail.com>
2026-07-24 21:43:14 -0300

    refactor(core): organize dtos into domain-specific folders

--- src/core/repositories/user.repository.ts
+++ src/core/repositories/user.repository.ts
@@ -1,5 +1,5 @@
-import { CreateUserDto } from "../dtos/create-user.dto";
-import { UpdateUserDto } from "../dtos/update-user.dto";
+import { CreateUserDto } from "../dtos/user/create-user.dto";
+import { UpdateUserDto } from "../dtos/user/update-user.dto";
 import { User } from "../models/user.model";
 
 export interface UserRepository {
