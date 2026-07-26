bd981f9ccca525a9a6e665db2db8e7590ca778bd
Pedro Marques <pedromarques391@gmail.com>
2026-07-26 17:34:56 -0300

    feat(auth): implement otp generation and verification endpoints

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,15 +1,61 @@
 import { UserRepo as UserRepository } from "@/repositories/user.repository";
 import { AuthService } from "@/services/auth.service";
 import { JWT } from "@/utils/JWT";
-import type { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+import type { FastifyInstance } from "fastify";
 
 export async function authRoutes(app: FastifyInstance) {
   const userRepository = new UserRepository();
   const jwtUtils = new JWT(app.jwt);
   const authService = new AuthService(userRepository, jwtUtils);
 
-  app.get("/", async (request: FastifyRequest, reply: FastifyReply) => {
-    const user = await authService.sendOTP("559185480955");
-    return user;
-  });
+  app.post<{ Body: { wa_id: string } }>(
+    "/login/otp",
+    async (request, reply) => {
+      try {
+        const { wa_id } = request.body;
+        console.log(wa_id);
+        const user = await authService.sendOTP(wa_id);
+
+        if (!user) {
+          throw new Error("Operation not permitted");
+        }
+
+        reply.send({
+          message: "OTP sent successfully",
+          user: user.user,
+        });
+      } catch (err) {
+        reply.status(400).send({
+          message: "Operation not permitted",
+        });
+      }
+    },
+  );
+  app.post<{ Body: { wa_id: string; otp: string } }>(
+    "/login/verify",
+    async (request, reply) => {
+      try {
+        const { wa_id, otp } = request.body;
+        if (!otp) {
+          throw new Error("Invalid or expired OTP");
+        }
+        const user = await authService.verifyOtpAndLogin(wa_id, otp);
+
+        reply.send({
+          message: "OTP verified successfully",
+          user: user.user,
+          token: user.token,
+        });
+      } catch (err) {
+        if (err instanceof Error) {
+          return reply.status(400).send({
+            message: err.message,
+          });
+        }
+        reply.status(400).send({
+          message: "Operation not permitted",
+        });
+      }
+    },
+  );
 }
