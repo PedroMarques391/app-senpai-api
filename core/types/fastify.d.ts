d229b84644f63d668747af6b1d6ec0befb9a8354
Pedro Marques <pedromarques391@gmail.com>
2026-07-26 16:06:30 -0300

    build(core): add type declarations for fastify and jwt

---
+++ core/types/fastify.d.ts
@@ -0,0 +1,19 @@
+import "@fastify/jwt";
+import "fastify";
+
+declare module "fastify" {
+  export interface FastifyInstance {
+    authenticate: (req: FastifyRequest, reply: FastifyReply) => Promise<void>;
+  }
+}
+
+declare module "@fastify/jwt" {
+  interface FastifyJWT {
+    user: {
+      wa_id: string;
+      name: string;
+      userName: string;
+      premium: boolean;
+    };
+  }
+}
