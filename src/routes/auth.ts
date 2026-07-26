367c447a6bfdcaa836a51a164da6ac71c4f17728
Pedro Marques <pedromarques391@gmail.com>
2026-07-26 16:06:50 -0300

    refactor(auth): update auth routes to use auth service

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,13 +1,15 @@
 import { UserRepo as UserRepository } from "@/repositories/user.repository";
-import { UserService } from "@/services/user.service";
+import { AuthService } from "@/services/auth.service";
+import { JWT } from "@/utils/JWT";
 import type { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 
 export async function authRoutes(app: FastifyInstance) {
   const userRepository = new UserRepository();
-  const userService = new UserService(userRepository);
+  const jwtUtils = new JWT(app.jwt);
+  const authService = new AuthService(userRepository, jwtUtils);
 
   app.get("/", async (request: FastifyRequest, reply: FastifyReply) => {
-    const user = await userService.findByWAId("559185480955");
+    const user = await authService.sendOTP("559185480955");
     return user;
   });
 }
