8fede1d29070cb75b10cf27e2a17a2baf9e1b460
Pedro Marques <pedromarques391@gmail.com>
2026-08-08 21:42:06 -0300

    feat(pack): add create and public listing endpoints to pack router

--- src/routes/pack.router.ts
+++ src/routes/pack.router.ts
@@ -2,6 +2,7 @@ import type { CreatePackDto } from "@/dtos";
 import type { FastifyInstance } from "fastify";
 import { PackRepository } from "@/repositories";
 import { PackService } from "@/services";
+import { ObjectId } from "mongodb";
 
 const packRepository = new PackRepository();
 const packService = new PackService(packRepository);
@@ -14,8 +15,17 @@ export async function packRoutes(app: FastifyInstance) {
     })
 
     app.post<{ Body: CreatePackDto }>("/create", async (request, reply) => {
+        const user = request.user
+        const idToObjectId = new ObjectId(user._id)
+        const pack = await packService.create(idToObjectId, user.userName, request.body)
         return reply.status(200).send({
             message: "Hello World from packs",
+        });
+    })
+    app.get("/public", async (request, reply) => {
+        const packs = await packService.findAll()
+        return reply.status(200).send({
+            packs
         });
     })
 
