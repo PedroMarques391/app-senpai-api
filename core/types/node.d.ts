f3a178a2aa0b6869c223af4e43df867401bb3cb6
Pedro Marques <pedromarques391@gmail.com>
2026-08-14 10:16:16 -0300

    feat(types): add node process.env type definitions

---
+++ core/types/node.d.ts
@@ -0,0 +1,15 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      MONGO_URI: string;
+      JWT_SECRET: string;
+      WHATSAPP_PHONE_ID: string;
+      WHATSAPP_TOKEN: string;
+      CLOUDINARY_CLOUD_NAME: string;
+      CLOUDINARY_API_KEY: string;
+      CLOUDINARY_API_SECRET: string;
+    }
+  }
+}
+
+export {};
