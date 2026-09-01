a1d7883e3dd214924550eeaef317db8f2533ea28
Pedro Marques <pedromarques391@gmail.com>
2026-09-01 19:18:08 -0300

    chore(app): update test endpoint in index.ts for queue verification

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { QueueFactory } from "@/factories";
 import { CloudinaryInitializer, MongoInitializer } from "@/init";
 import { authPlugin, errorPlugin, redisPlugin } from "@/plugin";
 import {
@@ -18,6 +19,7 @@ import {
   validatorCompiler,
   type ZodTypeProvider,
 } from "fastify-type-provider-zod";
+import z from "zod";
 
 const server = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -31,6 +33,43 @@ server.register(redisPlugin);
 server.register(authRoutes, { prefix: "/auth" });
 server.register(adminRouter, { prefix: "/admin" });
 
+//TODO remove this get, only test
+server.get(
+  "/",
+  {
+    schema: {
+      querystring: z.object({ id: z.string().optional() }),
+    },
+  },
+  async (request, reply) => {
+    const whatsapp = QueueFactory.getWhatsAppQueue();
+
+    if (request.query) {
+      const getQueue = await whatsapp.getQueue(request.query.id);
+      return reply.status(200).send({
+        queue: getQueue,
+      });
+    }
+
+    const job = await whatsapp.addJob(
+      "send-message",
+      {
+        number: "+5511998527431",
+        message: "Hello, how are you?",
+      },
+      {
+        attempts: 2,
+      },
+    );
+
+    return reply.status(200).send({
+      message: "Job added successfully",
+      success: true,
+      job,
+    });
+  },
+);
+
 server.register(async (app) => {
   app.addHook("onRequest", app.authenticate);
 
@@ -40,10 +79,6 @@ server.register(async (app) => {
       success: true,
       user: request.user,
     });
-  });
-
-  app.get("/", async () => {
-    return "This is senpai backend...";
   });
 
   app.register(inventoryRoutes, { prefix: "/inventory" });
