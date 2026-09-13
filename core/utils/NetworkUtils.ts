7e13bf03dd619ea32a3fbf3038c8418f86aeb6a8
Pedro Marques <pedromarques391@gmail.com>
2026-09-13 16:49:42 -0300

    refactor(utils): replace chained if-else with switch-case in NetworkUtils device parser
    - Decompose device parsing into getOperatingSystem and getBrowser
    - Utilize switch (true) pattern for cleaner evaluation
    - Prioritize Edge and Opera detection before Chrome in browser precedence

--- core/utils/NetworkUtils.ts
+++ core/utils/NetworkUtils.ts
@@ -22,20 +22,43 @@ export class NetworkUtils {
   static parseDevice(userAgent?: string): string {
     if (!userAgent) return "Dispositivo não identificado";
 
-    let os = "Dispositivo";
-    if (/windows/i.test(userAgent)) os = "Windows";
-    else if (/macintosh|mac os x/i.test(userAgent)) os = "macOS";
-    else if (/android/i.test(userAgent)) os = "Android";
-    else if (/iphone|ipad|ipod/i.test(userAgent)) os = "iOS";
-    else if (/linux/i.test(userAgent)) os = "Linux";
-
-    let browser = "Navegador";
-    if (/edg/i.test(userAgent)) browser = "Edge";
-    else if (/chrome|crios/i.test(userAgent)) browser = "Chrome";
-    else if (/firefox|fxios/i.test(userAgent)) browser = "Firefox";
-    else if (/safari/i.test(userAgent)) browser = "Safari";
-    else if (/opera|opr/i.test(userAgent)) browser = "Opera";
+    const os = this.getOperatingSystem(userAgent);
+    const browser = this.getBrowser(userAgent);
 
     return `${browser} (${os})`;
+  }
+
+  private static getOperatingSystem(ua: string): string {
+    switch (true) {
+      case /windows/i.test(ua):
+        return "Windows";
+      case /macintosh|mac os x/i.test(ua):
+        return "macOS";
+      case /android/i.test(ua):
+        return "Android";
+      case /iphone|ipad|ipod/i.test(ua):
+        return "iOS";
+      case /linux/i.test(ua):
+        return "Linux";
+      default:
+        return "Dispositivo";
+    }
+  }
+
+  private static getBrowser(ua: string): string {
+    switch (true) {
+      case /edg/i.test(ua):
+        return "Edge";
+      case /opera|opr/i.test(ua):
+        return "Opera";
+      case /chrome|crios/i.test(ua):
+        return "Chrome";
+      case /firefox|fxios/i.test(ua):
+        return "Firefox";
+      case /safari/i.test(ua):
+        return "Safari";
+      default:
+        return "Navegador";
+    }
   }
 }
